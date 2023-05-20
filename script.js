99ba5a290fa05ae7545bc71565b5e27b7ab1ecca
Kayo-b <kyo.bhz@gmail.com>
2023-05-20 17:33:43 +0200

    package correction

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
 
 app.post('/webhook', (req, res) => {
   console.log(req.body);
