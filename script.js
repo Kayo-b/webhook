a9a5cfdb9ed180d8494c3cd71d0d3b3401fa8435
Kayo-b <kyo.bhz@gmail.com>
2023-05-22 19:52:04 +0200

    hiding db password and changing param values

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
 const { Client } = require('pg');
 
+dotenv.config();
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +13,7 @@ const client = new Client( {
   host: 'mouse.db.elephantsql.com',
   port: 5432,
   user: 'cyrahcct',
-  password: 'W9f_HAWdmNSrjAB6xqyDTeqmuEr6eDaI',
+  password: process.env.ELSQLPASSWORD,
   database: 'cyrahcct'
 })
 console.log(client)
@@ -21,7 +23,7 @@ app.post('/webhook', (req, res) => {
   console.log(req.body);
   const data = req.body;
   client.query(
-    'INSERT INTO risk_form_data (name, company, email, machinesite, address, city, country, telefone) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [data.value1, data.value2,data.value3, data.value4, data.value5, data.value6, data.value7, data.value8])
+    'INSERT INTO risk_form_data (name, company, email, machinesite, address, city, country, telefone) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)', [data.fields.name.value, data.fields.company.value, data.fields.email.value, data.fields.machinesite.value, data.fields.address.value, data.fields.city.value, data.fields.country.value, data.fields.telefone.value])
   res.status(200).end();
   // client.query(`CREATE TABLE risk_form_data (
   //   id SERIAL PRIMARY KEY,
