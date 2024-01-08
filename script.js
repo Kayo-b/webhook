9001411fe0ab64f802f752e850786a1385fe14c3
Kayo-b <kyo.bhz@gmail.com>
2024-01-08 20:36:21 +0100

    Add node-fetch dependency

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+import fetch from 'node-fetch';
+
 require('dotenv').config()
 const express = require('express');
-const fetch = require('node-fetch'); 
 const app = express();
 const { Client } = require('pg');
 
